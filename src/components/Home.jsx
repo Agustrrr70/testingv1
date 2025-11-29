1a3c44fa8b5869b3a000a00e3cf6220c09ee66fb
Agustrrr70 <2111016110011@mhs.ulm.ac.id>
2025-11-29 13:04:43 +0800

    i

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 
 const Home = () => {
   useEffect(() => {
-    // Hitung tinggi layar aktual (bukan 100vh palsu milik mobile browser)
     const setVH = () => {
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -10,42 +9,84 @@ const Home = () => {
 
     setVH();
     window.addEventListener("resize", setVH);
-
-    return () => {
-      window.removeEventListener("resize", setVH);
-    };
+    return () => window.removeEventListener("resize", setVH);
   }, []);
+
+  const menuItems = [
+    { img: "/images/surah.png" },
+    { img: "/images/wirid.png" },
+    { img: "/images/doa.png" },
+    { img: "/images/khutbah.png" },
+    { img: "/images/dalail.png" },
+    { img: "/images/tasbih.png" },
+    { img: "/images/burdah.png" },
+    { img: "/images/simt.png" },
+    { img: "/images/ma.png" },
+  ];
 
   return (
     <div
       style={{
         width: "100vw",
-        height: "calc(var(--vh) * 100)",  // 100% tinggi layar HP asli
+        height: "calc(var(--vh) * 100)",
         backgroundImage: "url(/images/backgroundmain.png)",
-        backgroundSize: "100% 100%",       // stretch agar selalu penuh
-        backgroundPosition: "center",
+        backgroundSize: "100% 100%",
         backgroundRepeat: "no-repeat",
-        overflow: "hidden",                // pastikan tidak bisa scroll
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "flex-start",
+        overflow: "hidden",
+        position: "relative",
       }}
     >
-      {/* ---- Contoh konten di atas background ---- */}
+      {/* KONTAINER-ALL */}
       <div
         style={{
-          marginTop: "20px",
-          color: "white",
-          fontSize: "24px",
-          fontWeight: "bold",
-          textShadow: "0 2px 6px rgba(0,0,0,0.6)",
+          position: "absolute",
+          bottom: "11%",
+          left: 0,
+          width: "100%",
+          height: "55vh",
+          borderRadius: "20px 20px 0 0",
+          padding: "20px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxSizing: "border-box",
         }}
       >
-
+        {/* GRID WRAPPER yang terkunci di dalam kontainer-all */}
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "grid",
+            gridTemplateColumns: "repeat(3, 1fr)",
+            gridTemplateRows: "repeat(3, 1fr)",
+            gap: "15px",
+          }}
+        >
+          {menuItems.map((item, index) => (
+            <div
+              key={index}
+              style={{
+                borderRadius: "16px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                overflow: "hidden",
+              }}
+            >
+              <img
+                src={item.img}
+                style={{
+                  width: "70%",
+                  height: "70%",
+                  objectFit: "cover",
+                  borderRadius: "40px", // 🔵 BIKIN IKON BUNDAR
+                }}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-
-      {/* Tambahkan menu / tombol / elemen lain di sini */}
     </div>
   );
 };
