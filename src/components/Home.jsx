81d7e899b8b9c2d193f097448375796cebf44bf4
Agustrrr70 <2111016110011@mhs.ulm.ac.id>
2025-11-29 12:10:42 +0800

    tes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    // Kunci scroll ketika halaman ini terbuka
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      // Kembalikan seperti semula jika user keluar halaman
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
     <div
       style={{
         width: "100vw",
         height: "100vh",
         backgroundImage: 'url(/images/backgroundmain.png)',
-        backgroundSize: "100% 100%",   // stretch agar selalu memenuhi layar
-        backgroundPosition: "center",
+        backgroundSize: "100% 100%",   // stretch agar penuh
         backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        overflow: "hidden",
       }}
     >
-      {/* Letakkan elemen lain di sini */}
     </div>
   );
 };
